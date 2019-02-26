8b5236458d64c0613a74577f7f5e084d2c3f94a8
unknown <eriznis@gmail.com>
2019-02-26 17:58:38 -0500

    Data imported and wrapper component created

---
+++ apple-nav/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavWrapper from './components/NavWrapper'
+import { navNames, macData, ipadData, iphoneData, watchData, tvData, musicData} from './AppleIconsData'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      names: [],
+      mac: [],
+      ipad: [],
+      iphone: [],  
+      watch: [],
+      tv: [],
+      music: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState ({names: navNames})
+    this.setState ({mac: macData})
+    this.setState ({ipad: ipadData})
+    this.setState ({iphone: iphoneData})
+    this.setState ({watch: watchData})
+    this.setState ({tv: tvData})
+    this.setState ({music: musicData})
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <NavWrapper />
+          
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ apple-nav/src/components/NavWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavWrapper() {
+    return (
+        <div className='wrapper'></div>
+    )
+}
+
+export default NavWrapper
